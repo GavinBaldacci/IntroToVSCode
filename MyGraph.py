4d7117d37327770b6509726631a5b4fe281190af
Gavin Baldacci <gavin_baldacci1@baylor.edu>
2024-08-29 16:45:25 -0500

    Initial commit

---
+++ MyGraph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+
+plt.show()
+
+#1. create your VE - py-3-m venv ______ (myvenv)
+#2. activate your VE - myvenv/Scripts/activate
+#3. install 3rd party library - pip3 install _______(matplotlib)
+# Difference between: Version Control - version as a whole, website includes logos, images, text, etc. 
+# Source Control - development of application, source code that makes application work
+# ^ Used to track projects
---
+++ Python1.py
@@ -0,0 +1,13 @@
+a=1
+b=2
+c=a+b
+
+for x in range(10):
+    y=x*c
+
+
+message="Hello world!"
+
+print(message)
+#The debugger stops prior to exectutin(or the line its on)
+#When continuing, it will keep going through the for loop, then will move on
